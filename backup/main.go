4e0649822d00a061df12a7572a1fc13ff3b45280
vmilovanovicc <codingvesna1@gmail.com>
2024-02-05 18:31:16 +0100

    Provision DynamoDB table

--- backup/main.go
+++ backup/main.go
@@ -1,20 +1,61 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/dynamodb"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucket, err := s3.NewBucket(ctx, "my-bucket", nil)
+		// Provide a DynamoDB table resource.
+		tbl, err := dynamodb.NewTable(ctx, "demo-dynamodb-table", &dynamodb.TableArgs{
+			Attributes: dynamodb.TableAttributeArray{
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("CustomerId"),
+					Type: pulumi.String("S"),
+				},
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("ProductName"),
+					Type: pulumi.String("S"),
+				},
+				&dynamodb.TableAttributeArgs{
+					Name: pulumi.String("PurchaseAmount"),
+					Type: pulumi.String("N"),
+				},
+			},
+			BillingMode: pulumi.String("PROVISIONED"),
+			GlobalSecondaryIndexes: dynamodb.TableGlobalSecondaryIndexArray{
+				&dynamodb.TableGlobalSecondaryIndexArgs{
+					HashKey: pulumi.String("ProductName"),
+					Name:    pulumi.String("ProductNameIndex"),
+					NonKeyAttributes: pulumi.StringArray{
+						pulumi.String("CustomerId"),
+					},
+					ProjectionType: pulumi.String("INCLUDE"),
+					RangeKey:       pulumi.String("PurchaseAmount"),
+					ReadCapacity:   pulumi.Int(10),
+					WriteCapacity:  pulumi.Int(10),
+				},
+			},
+			HashKey:      pulumi.String("CustomerId"),
+			RangeKey:     pulumi.String("ProductName"),
+			ReadCapacity: pulumi.Int(20),
+			Tags: pulumi.StringMap{
+				"Environment": pulumi.String("dev"),
+				"Name":        pulumi.String("dynamodb-table-1"),
+			},
+			Ttl: &dynamodb.TableTtlArgs{
+				AttributeName: pulumi.String("TimeToExist"),
+				Enabled:       pulumi.Bool(false),
+			},
+			WriteCapacity: pulumi.Int(20),
+		})
 		if err != nil {
 			return err
 		}
-
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
+		// Export output data.
+		ctx.Export("demoTableArn", tbl.Arn)
 		return nil
 	})
+
 }
